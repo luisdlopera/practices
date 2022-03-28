8df386c480d48f8215a3c0f503dab039eaa0f22e
Luis David Lopera <luis.lopera28@gmail.com>
2022-03-28 12:23:19 -0500

    1

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ const paragraph = document.getElementById("paragraph");
 
 const item = document.createElement("LI");
 
-let text = "esto es un texto";
+item.innerHTML = "aqui estoy yo ";
 
 const text2 = document.createTextNode("ahora si o no ");
-
-item.appendChild(text2);
 
 paragraph.appendChild(item);
 
